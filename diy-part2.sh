886e0eba35faf4d38f7c3ee4698e7c6348cac7f2
whfweb <40481696+whfweb@users.noreply.github.com>
2021-06-24 12:59:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,14 +9,14 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 # Modify WiFi ON
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # Modify default SSID
-sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #add packages
-git clone https://github.com/shmily103/Panzy.git package/Panzy
+git clone https://github.com/whfweb/Panzy.git package/Panzy
 # Modify Automount
 \cp -rf package/Panzy/mount.hotplug package/system/fstools/files
 # fix unblockmusic
