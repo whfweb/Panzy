b2777ffefba2968b25bc1385a3f8f0d50c682ffc
whfweb <40481696+whfweb@users.noreply.github.com>
2024-04-08 10:39:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,7 +7,7 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-#
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
