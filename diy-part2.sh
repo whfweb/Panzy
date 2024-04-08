91d90a35a8238d7962b8e4cde0fbf4083f77e326
whfweb <40481696+whfweb@users.noreply.github.com>
2024-04-08 12:22:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#============================================================
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
-#============================================================
+#=============================================================
 #add packages
 git clone https://github.com/shmily103/Panzy.git package/Panzy
 
